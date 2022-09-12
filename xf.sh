d334c2277b446390f411f8ace55ecfdb67eeaf04
jacksmor <hyunelise@gmail.com>
2022-09-12 14:11:16 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -8,26 +8,13 @@ DOWNLOAD_PATH="/tmp/v2ray"
 mkdir -p ${DOWNLOAD_PATH}
 cd ${DOWNLOAD_PATH} || exit
 
-TAG=$(wget --no-check-certificate -qO- https://api.github.com/repos/v2fly/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)
-if [ -z "${TAG}" ]; then
-    echo "Error: Get v2ray latest version failed" && exit 1
-fi
-echo "The v2ray latest version: ${TAG}"
-
 # Download files
 V2RAY_FILE="v2ray-linux-${ARCH}.zip"
 DGST_FILE="v2ray-linux-${ARCH}.zip.dgst"
-echo "Downloading binary file: ${V2RAY_FILE}"
-echo "Downloading binary file: ${DGST_FILE}"
 
 # TAG=$(wget -qO- https://raw.githubusercontent.com/v2fly/docker/master/ReleaseTag | head -n1)
-wget -O ${DOWNLOAD_PATH}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2RAY_FILE} >/dev/null 2>&1
-wget -O ${DOWNLOAD_PATH}/v2ray.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${DGST_FILE} >/dev/null 2>&1
-
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to download binary file: ${V2RAY_FILE} ${DGST_FILE}" && exit 1
-fi
-echo "Download binary file: ${V2RAY_FILE} ${DGST_FILE} completed"
+wget -O ${DOWNLOAD_PATH}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v4.45.2/v2ray-linux-64.zip
+wget -O ${DOWNLOAD_PATH}/v2ray.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/v4.45.2/v2ray-linux-64.zip.dgst
 
 # Check SHA512
 LOCAL=$(openssl dgst -sha512 v2ray.zip | sed 's/([^)]*)//g')
@@ -41,8 +28,8 @@ fi
 
 # Prepare
 echo "Prepare to use"
-unzip v2ray.zip && chmod +x v2ray
-mv v2ray /usr/bin/
+unzip v2ray.zip && chmod +x v2ray v2ctl
+mv v2ray v2ctl /usr/bin/
 mv geosite.dat geoip.dat /usr/local/share/v2ray/
 # mv config.json /etc/v2ray/config.json
 
